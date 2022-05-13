9d30b141295f8f546e3c97b5b883758fbc5bbe4b
aarish <aarish.jerry@gmail.com>
2022-05-13 23:15:26 +0530

    first commit

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedTab = "One"
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $selectedTab) {
+            Text("Tab 1")
+                .onTapGesture {
+                    selectedTab = "Two"
+                }
+                .tabItem {
+                    Label("One", systemImage: "star")
+                }
+                .tag("One")
+
+            Text("Tab 2")
+                .tabItem {
+                    Label("Two", systemImage: "circle")
+                }
+                .tag("Two")
+        }
     }
 }
 
