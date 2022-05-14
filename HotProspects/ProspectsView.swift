8883418f86a465d0bdd0b0c46fdfc25d8561ef87
aarish <aarish.jerry@gmail.com>
2022-05-14 11:14:53 +0530

    Notifications added

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 import CodeScanner
+import UserNotifications
+
 struct ProspectsView: View {
     enum FilterType {
         case none, contacted, uncontacted
@@ -35,12 +37,20 @@ struct ProspectsView: View {
                             }
                             .tint(.blue)
                         } else {
+                            
                             Button {
                                 prospects.toggle(prospect)
                             } label: {
                                 Label("Mark Contacted", systemImage: "person.crop.circle.fill.badge.checkmark")
                             }
                             .tint(.green)
+                            Button {
+                                addNotification(for: prospect)
+                            } label: {
+                                Label("Remind Me", systemImage: "bell")
+                            }
+                            .tint(.orange)
+                            
                         }
                     }
                 }
@@ -97,6 +107,38 @@ struct ProspectsView: View {
             
         case .failure(let error):
             print("Scanning failed: \(error.localizedDescription)")
+        }
+    }
+    
+    func addNotification(for prospect: Prospect) {
+        let center = UNUserNotificationCenter.current()
+
+        let addRequest = {
+            let content = UNMutableNotificationContent()
+            content.title = "Contact \(prospect.name)"
+            content.subtitle = prospect.emailAddress
+            content.sound = UNNotificationSound.default
+
+            var dateComponents = DateComponents()
+            dateComponents.hour = 9
+//            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            center.add(request)
+        }
+
+        center.getNotificationSettings { settings in
+            if settings.authorizationStatus == .authorized {
+                addRequest()
+            } else {
+                center.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        addRequest()
+                    } else {
+                        print("D'oh")
+                    }
+                }
+            }
         }
     }
 }
